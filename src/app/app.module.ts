c03d7c18cf8b63bbab009cb75cef0442d6e978a5
andrelius <andrelius.quasar@gmail.com>
2019-05-18 20:24:51 -0300

    Criando e alinhando parte superior do site

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { EmprestimoComponent } from './home/emprestimo/emprestimo.component';
+import { CadastroComponent } from './home/cadastro/cadastro.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    EmprestimoComponent,
+    CadastroComponent
   ],
   imports: [
     BrowserModule
